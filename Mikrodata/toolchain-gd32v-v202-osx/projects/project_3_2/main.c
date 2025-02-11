9b1c586fdf16ed2bb09006ffb1e98121eed121ab
Robin Barwari <70271799+Robin-Barwari@users.noreply.github.com>
2025-02-11 21:15:28 +0100

    sui

--- Mikrodata/toolchain-gd32v-v202-osx/projects/project_3_2/main.c
+++ Mikrodata/toolchain-gd32v-v202-osx/projects/project_3_2/main.c
@@ -3,95 +3,85 @@
 #include "pwm.h"
 #include <stdio.h>
 
-void run(int stuff[], int *item, int row[],int target,int *end);
+void run(int stuff[], int *item, int row[], int target, int *end, int *point);
 
 int main(void)
 {
 
-    int arr[40] = {     /// this for map
-    1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 
-    7, 7, 7, 7, 1, 1, 64, 54, 64, 64,
-    5, 1, 1, 1, 1, 1, 1, 125, 1, 200,
-    1, 40, 60, 1, 120, 1, 7, 2, 4, 7
-};
+  int arr[40] = {/// this for map
+                 1, 1, 1, 1, 2, 2, 2, 2, 4, 4,
+                 7, 7, 7, 7, 1, 1, 64, 54, 64, 64,
+                 5, 1, 1, 1, 1, 1, 1, 125, 1, 200,
+                 1, 40, 60, 1, 120, 1, 7, 2, 4, 7};
 
-   
-  int ms = 0, end = 1, item = 0,sum = 0,target=0;
+  int ms = 0, end = 1, item = 0, sum = 0, target = 0, point = 0;
   int lookUpTbl[16] = {15, 11, 0, 14, 10, 9, 8, 7, 10, 6, 5, 4, 10, 3, 2, 1}; // alla tangentbord i ordning
-  int path[] = {1,2,4,8,16,32,64,128,256};    // 8x8 data 
-  int row[8] = {0};    // will make the flow uppdate
-  
-  
+  int path[] = {1, 2, 4, 8, 16, 32, 64, 128, 256};                            // 8x8 data
+  int row[8] = {0};                                                           // will make the flow uppdate
+
   t5omsi();  // Initialize timer5 1kHz
   colinit(); // Initialize column toolbox
   l88init(); // Initialize 8*8 led toolbox
   keyinit(); // Initialize keyboard toolbox
   ADC3powerUpInit(1);
- 
+
   while (1)
   {
-    int ratt = (read_adc()*7)/4059; // 0... 4059 I will make it 0 till 7
-    ///read_adc()///  Den function kommer att omvandla analog till digital genom adc.c
+    int ratt = (read_adc() * 7) / 4059; // 0... 4059 I will make it 0 till 7
+    /// read_adc()///  Den function kommer att omvandla analog till digital genom adc.c
 
     if (t5expq())
-    {                   
+    {
       l88row(colset());
-      
-       if(end)
-       {   
-         ms++;
-         target = path[ratt];
-         l88mem(7,path[ratt]);    // the target                      
-            if (ms==500){  // this will make the map flow
-               run(arr,&item,row,target,&end);
-              ms=0;  
-            }        
-       }
+
+      if (end)
+      {
+        ms++;
+        target = path[ratt];
+        l88mem(7, path[ratt]); // the target
+        if (ms == 500)
+        { // this will make the map flow
+          run(arr, &item, row, target, &end, &point);
+          ms = 0;
+        }
+      }
       else
       {
 
         // Sad face in end
-         for (int i = 0; i < 8; i++)
-      {
-        if(i==0 || i == 7){ l88mem(i,255);}
-        else if (i == 2)
+        for (int i = 0; i < 8; i++)
         {
-           l88mem(i,165);
+          l88mem(i, 0); // clear the map
         }
-        else if (i == 4)
-        {
-          l88mem(i,153);
-        }
-         else if (i == 5)
-        {
-          l88mem(i,165);
-        }
-        else{l88mem(i,129);};
+
+        l88mem(4, point); // visa point binärt
       }
-      }
-     
-    
-    
     }
   }
 }
-void run(int stuff[], int *item, int row[],int target,int *end) {
-    
 
-    if (*item == 40) {     /// make array looper på sig själv 
-        *item = 0;     
-    }
+void run(int stuff[], int *item, int row[], int target, int *end, int *point)
+{
 
-   
-    for (int i = 7; i > 0; i--) {  // den kommer att uppdetera row array
-        row[i] = row[i - 1];  
-    }
+  if (*item == 40)
+  { /// make array looper på sig själv
+    *item = 0;
+  }
 
-    
-    row[0] = stuff[*item];    // nästa värder för row 
-    for (int i = 0; i < 8; i++) {   // uppdeterar map 
-        l88mem(i, row[i]);
-        if (target & row[7]) {*(end) =0;};     // Om spelare kollidera med hindrar sluta spelet 
-    }
-    (*item)++;     // uppdetera map att den kommer att loop igen
+  for (int i = 7; i > 0; i--)
+  { // den kommer att uppdetera row array
+    row[i] = row[i - 1];
+  }
+
+  row[0] = stuff[*item]; // nästa värder för row
+  for (int i = 0; i < 8; i++)
+  { // uppdeterar map
+    l88mem(i, row[i]);
+    if (target & row[7])
+    {
+      *(end) = 0;
+    }; // Om spelare kollidera med hindrar sluta spelet
+  }
+  (*point)++; // uppdetera point
+  (*item)++;  // uppdetera map att den kommer att loop igen
 }
